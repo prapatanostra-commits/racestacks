04b02f9dd9d811ca9086cca1a305558308047340
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-18 16:49:19 +0700

    fix: resolve processing issue

--- frontend/app/RaceStacksApp.tsx
+++ frontend/app/RaceStacksApp.tsx
@@ -105,3 +105,4 @@ export default function RaceStacksApp() {
 // internal sync 36 at Jum 17 Apr 2026 12:36:52 WIB
 // internal sync 47 at Jum 17 Apr 2026 13:42:39 WIB
 // internal sync 71 at Sab 18 Apr 2026 15:42:46 WIB
+// internal sync 81 at Sab 18 Apr 2026 16:49:19 WIB

50adc127c8d9bcc57549ce552cbc0955d994a60e
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-19 00:04:50 +0700

    feat: implement contract utility abstraction

--- frontend/app/RaceStacksApp.tsx
+++ frontend/app/RaceStacksApp.tsx
@@ -113,3 +113,4 @@ export default function RaceStacksApp() {
 // internal sync 117 at Sab 18 Apr 2026 20:22:44 WIB
 // internal sync 124 at Sab 18 Apr 2026 21:08:08 WIB
 // internal sync 125 at Sab 18 Apr 2026 21:16:05 WIB
+// internal sync 154 at Min 19 Apr 2026 00:04:50 WIB

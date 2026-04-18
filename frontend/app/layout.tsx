21cacf498fcce42c3cc9093ad23b1694c2d8ffa1
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-19 05:38:02 +0700

    fix: correct transaction logic

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -45,3 +45,4 @@ export default function RootLayout({
 // internal sync 128 at Sab 18 Apr 2026 21:31:06 WIB
 // internal sync 183 at Min 19 Apr 2026 02:59:53 WIB
 // internal sync 205 at Min 19 Apr 2026 05:20:04 WIB
+// internal sync 208 at Min 19 Apr 2026 05:38:02 WIB

904819bb4430a0128da4ee2e5cd3fdd585973c4d
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-19 17:17:16 +0700

    refactor: clean architecture layers

--- frontend/app/RaceStacksApp.tsx
+++ frontend/app/RaceStacksApp.tsx
@@ -121,3 +121,4 @@ export default function RaceStacksApp() {
 // internal sync 284 at Min 19 Apr 2026 13:21:59 WIB
 // internal sync 285 at Min 19 Apr 2026 13:26:24 WIB
 // internal sync 286 at Min 19 Apr 2026 13:32:20 WIB
+// internal sync 9 at Min 19 Apr 2026 17:17:16 WIB

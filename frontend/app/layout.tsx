412b349fa22cf6949f3f271789d66dac5c17c9b6
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-19 21:43:26 +0700

    fix: resolve incorrect mapping

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -52,3 +52,4 @@ export default function RootLayout({
 // internal sync 5 at Min 19 Apr 2026 16:52:57 WIB
 // internal sync 13 at Min 19 Apr 2026 17:42:34 WIB
 // internal sync 21 at Min 19 Apr 2026 18:31:06 WIB
+// internal sync 53 at Min 19 Apr 2026 21:43:26 WIB

dcba03679848f7c84bbe514ae6a263fcf782cb1d
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-19 09:16:04 +0700

    refactor: simplify architecture

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -47,3 +47,4 @@ export default function RootLayout({
 // internal sync 205 at Min 19 Apr 2026 05:20:04 WIB
 // internal sync 208 at Min 19 Apr 2026 05:38:02 WIB
 // internal sync 210 at Min 19 Apr 2026 05:49:46 WIB
+// internal sync 245 at Min 19 Apr 2026 09:16:04 WIB

cca9627c08210f0eb0ab2b3ed505a6a94d0bf085
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-19 07:17:19 +0700

    chore: remove deprecated code

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -28,3 +28,4 @@ export default function Home() {
 // internal sync 181 at Min 19 Apr 2026 02:46:43 WIB
 // internal sync 191 at Min 19 Apr 2026 03:51:37 WIB
 // internal sync 194 at Min 19 Apr 2026 04:08:49 WIB
+// internal sync 224 at Min 19 Apr 2026 07:17:19 WIB

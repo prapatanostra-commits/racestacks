cce2563c0bca294e7cd41c2f6faf41f47dac0525
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-20 22:07:12 +0700

    fix: adjust logical conditions

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -51,3 +51,4 @@ export default function Home() {
 // internal sync 90 at Sen 20 Apr 2026 21:09:49 WIB
 // internal sync 95 at Sen 20 Apr 2026 21:30:14 WIB
 // internal sync 96 at Sen 20 Apr 2026 21:33:23 WIB
+// internal sync 103 at Sen 20 Apr 2026 22:07:12 WIB

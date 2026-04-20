ad93460fff067c2c8adbe24cad91a20852082622
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 01:10:01 +0700

    feat: update mission contract behavior

--- frontend/app/RaceStacksApp.tsx
+++ frontend/app/RaceStacksApp.tsx
@@ -141,3 +141,4 @@ export default function RaceStacksApp() {
 // internal sync 62 at Sen 20 Apr 2026 19:02:34 WIB
 // internal sync 68 at Sen 20 Apr 2026 19:29:41 WIB
 // internal sync 83 at Sen 20 Apr 2026 20:38:28 WIB
+// internal sync 144 at Sel 21 Apr 2026 01:10:01 WIB

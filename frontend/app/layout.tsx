c51121027cf816476f8dd9b4a3626884f2fb5638
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-20 16:01:48 +0700

    feat: improve contract composition

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -60,3 +60,4 @@ export default function RootLayout({
 // internal sync 166 at Sen 20 Apr 2026 09:04:39 WIB
 // internal sync 200 at Sen 20 Apr 2026 12:32:32 WIB
 // internal sync 210 at Sen 20 Apr 2026 13:24:46 WIB
+// internal sync 21 at Sen 20 Apr 2026 16:01:48 WIB

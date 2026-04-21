bfecccd145cec8ce700bfd59fe2bc5b8303343d1
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 10:38:55 +0700

    refactor: optimize resource handling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -57,3 +57,4 @@ export default function Home() {
 // internal sync 172 at Sel 21 Apr 2026 03:09:00 WIB
 // internal sync 201 at Sel 21 Apr 2026 05:27:33 WIB
 // internal sync 268 at Sel 21 Apr 2026 10:26:45 WIB
+// internal sync 271 at Sel 21 Apr 2026 10:38:55 WIB

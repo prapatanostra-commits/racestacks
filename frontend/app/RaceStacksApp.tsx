8eacc6404d4d016dbfe7df5682c0c6959ed10f99
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 21:28:34 +0700

    chore: synchronize configuration files

--- frontend/app/RaceStacksApp.tsx
+++ frontend/app/RaceStacksApp.tsx
@@ -153,3 +153,4 @@ export default function RaceStacksApp() {
 // internal sync 82 at Sel 21 Apr 2026 19:48:21 WIB
 // internal sync 84 at Sel 21 Apr 2026 19:57:36 WIB
 // internal sync 95 at Sel 21 Apr 2026 20:45:59 WIB
+// internal sync 104 at Sel 21 Apr 2026 21:28:34 WIB

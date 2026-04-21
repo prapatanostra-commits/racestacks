b40ea872d852c4c40daf1010ca7bc8630ac4d19b
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-22 03:53:10 +0700

    chore: clean code formatting

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -86,3 +86,4 @@ export default function RootLayout({
 // internal sync 128 at Sel 21 Apr 2026 23:17:12 WIB
 // internal sync 154 at Rab 22 Apr 2026 01:13:09 WIB
 // internal sync 179 at Rab 22 Apr 2026 03:11:05 WIB
+// internal sync 188 at Rab 22 Apr 2026 03:53:10 WIB

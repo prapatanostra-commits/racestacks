33318184b50c218ba30043c28eee50cea43120fa
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-22 01:13:09 +0700

    feat: implement logic upgrade

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -84,3 +84,4 @@ export default function RootLayout({
 // internal sync 109 at Sel 21 Apr 2026 21:51:19 WIB
 // internal sync 122 at Sel 21 Apr 2026 22:50:56 WIB
 // internal sync 128 at Sel 21 Apr 2026 23:17:12 WIB
+// internal sync 154 at Rab 22 Apr 2026 01:13:09 WIB

04c0028460342c36e48d03eacadfcbec5a45b0da
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-23 05:17:59 +0700

    docs: expand usage examples

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -79,3 +79,4 @@ export default function Home() {
 // internal sync 143 at Kam 23 Apr 2026 01:24:37 WIB
 // internal sync 148 at Kam 23 Apr 2026 01:46:22 WIB
 // internal sync 149 at Kam 23 Apr 2026 01:52:18 WIB
+// internal sync 192 at Kam 23 Apr 2026 05:17:59 WIB

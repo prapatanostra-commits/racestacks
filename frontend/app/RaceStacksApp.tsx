d49657a4ec2fedc58f57cb1814d67c9e61058564
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-23 11:42:48 +0700

    fix: resolve incorrect mapping

--- frontend/app/RaceStacksApp.tsx
+++ frontend/app/RaceStacksApp.tsx
@@ -186,3 +186,4 @@ export default function RaceStacksApp() {
 // internal sync 22 at Kam 23 Apr 2026 10:06:59 WIB
 // internal sync 29 at Kam 23 Apr 2026 10:33:36 WIB
 // internal sync 41 at Kam 23 Apr 2026 11:20:48 WIB
+// internal sync 46 at Kam 23 Apr 2026 11:42:48 WIB

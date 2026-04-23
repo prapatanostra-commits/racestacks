67f28a404ff7396a18ffc5d4ee40607564bb8ea5
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-23 20:53:43 +0700

    docs: enhance explanation details

--- frontend/app/RaceStacksApp.tsx
+++ frontend/app/RaceStacksApp.tsx
@@ -191,3 +191,4 @@ export default function RaceStacksApp() {
 // internal sync 19 at Kam 23 Apr 2026 14:14:32 WIB
 // internal sync 25 at Kam 23 Apr 2026 14:40:29 WIB
 // internal sync 43 at Kam 23 Apr 2026 15:51:31 WIB
+// internal sync 117 at Kam 23 Apr 2026 20:53:43 WIB

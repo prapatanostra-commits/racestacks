dca75877536df250dbb199f1bd30a551f5344545
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-23 18:28:42 +0700

    docs: update module documentation

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -102,3 +102,4 @@ export default function RootLayout({
 // internal sync 55 at Kam 23 Apr 2026 16:40:25 WIB
 // internal sync 69 at Kam 23 Apr 2026 17:38:23 WIB
 // internal sync 80 at Kam 23 Apr 2026 18:25:21 WIB
+// internal sync 81 at Kam 23 Apr 2026 18:28:42 WIB

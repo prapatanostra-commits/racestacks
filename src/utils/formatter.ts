4c0b26759b275f12c352e451ca313e7d2800a532
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 13:59:40 +0700

    feat(ui): add loading states for transaction broadcasting (#2493)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -496,3 +496,4 @@ export const VERSION_2472 = '2472';
 export const VERSION_2477 = '2477';
 export const VERSION_2480 = '2480';
 export const VERSION_2482 = '2482';
+export const VERSION_2493 = '2493';

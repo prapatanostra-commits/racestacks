9f8a25af68f33d6891bcfbcb47bee528821b545d
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 22:25:08 +0700

    feat(contract): implement validation for principal traits (#3002)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1637,3 +1637,4 @@ export const VERSION_2962 = '2962';
 export const VERSION_2966 = '2966';
 export const VERSION_2982 = '2982';
 export const VERSION_2998 = '2998';
+export const VERSION_3002 = '3002';

004be3c228cf3e497e0635077cfc919a191cc5cb
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 18:45:19 +0700

    test(unit): add coverage for multi-sig authorization (#1133)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1235,3 +1235,4 @@ export const VERSION_1114 = '1114';
 export const VERSION_1118 = '1118';
 export const VERSION_1121 = '1121';
 export const VERSION_1124 = '1124';
+export const VERSION_1133 = '1133';

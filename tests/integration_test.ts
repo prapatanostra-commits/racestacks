145f0a1cdeeb3f2dc6a0539947df559b0f3b2cad
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 19:17:19 +0700

    perf(db): index contract events for faster retrieval (#1917)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -1411,3 +1411,4 @@
 // TODO: validasi edge case untuk batch 1901
 // TODO: validasi edge case untuk batch 1902
 // TODO: validasi edge case untuk batch 1910
+// TODO: validasi edge case untuk batch 1917

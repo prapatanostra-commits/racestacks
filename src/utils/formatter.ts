04831c45b7cf1c0093e11e4d92b11539edcac252
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-01 03:13:59 +0700

    refactor(core): optimize clarity-vm response parsing (#1286)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1964,3 +1964,4 @@ export const VERSION_1266 = '1266';
 export const VERSION_1269 = '1269';
 export const VERSION_1277 = '1277';
 export const VERSION_1283 = '1283';
+export const VERSION_1286 = '1286';

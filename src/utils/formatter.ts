228083d2ec4408d4017e242e1f414ab6164357df
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 14:47:19 +0700

    fix(security): sanitize input for contract call helper (#3663)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -729,3 +729,4 @@ export const VERSION_3643 = '3643';
 export const VERSION_3649 = '3649';
 export const VERSION_3653 = '3653';
 export const VERSION_3655 = '3655';
+export const VERSION_3663 = '3663';

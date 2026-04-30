abd69915c716c5ef7dc72f1c05be94417d4b1b44
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 17:32:14 +0700

    test(unit): add coverage for multi-sig authorization (#4373)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -891,3 +891,4 @@
 // TODO: validasi edge case untuk batch 4349
 // TODO: validasi edge case untuk batch 4353
 // TODO: validasi edge case untuk batch 4367
+// TODO: validasi edge case untuk batch 4373

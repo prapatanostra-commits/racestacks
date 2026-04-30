4b19f0aee5b4d286e7d3eb3ef3ebdc1cd1898c7e
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 17:52:25 +0700

    feat(ui): add loading states for transaction broadcasting (#4854)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -1001,3 +1001,4 @@
 // TODO: validasi edge case untuk batch 4841
 // TODO: validasi edge case untuk batch 4842
 // TODO: validasi edge case untuk batch 4852
+// TODO: validasi edge case untuk batch 4854

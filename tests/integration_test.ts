5537861931c81269ff8cfcecfc4f973fd86119f4
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-30 19:28:24 +0700

    fix(security): sanitize input for contract call helper (#2101)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -1450,3 +1450,4 @@
 // TODO: validasi edge case untuk batch 2093
 // TODO: validasi edge case untuk batch 2096
 // TODO: validasi edge case untuk batch 2098
+// TODO: validasi edge case untuk batch 2101

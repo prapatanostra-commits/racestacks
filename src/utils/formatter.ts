6a63a0869d8477d324b90a3a6be4742448d3d4fc
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-01 19:23:11 +0700

    fix(api): resolve nonce mismatch in broadcast loop (#4565)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -4649,3 +4649,4 @@ export const VERSION_4558 = '4558';
 export const VERSION_4559 = '4559';
 export const VERSION_4561 = '4561';
 export const VERSION_4562 = '4562';
+export const VERSION_4565 = '4565';

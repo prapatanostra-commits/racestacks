492713eb960728dd7f8bdf7eeff174b6a64e22a3
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-01 10:58:06 +0700

    style(frontend): align dashboard margins with design spec (#1492)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -3016,3 +3016,4 @@ export const VERSION_1450 = '1450';
 export const VERSION_1451 = '1451';
 export const VERSION_1452 = '1452';
 export const VERSION_1455 = '1455';
+export const VERSION_1492 = '1492';

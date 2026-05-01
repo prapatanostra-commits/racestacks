c3291bec550ab29453a7c393e57f1a8252c801ba
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-01 11:33:59 +0700

    fix(api): resolve nonce mismatch in broadcast loop (#2378)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -3187,3 +3187,4 @@
 // TODO: validasi edge case untuk batch 2359
 // TODO: validasi edge case untuk batch 2363
 // TODO: validasi edge case untuk batch 2365
+// TODO: validasi edge case untuk batch 2378

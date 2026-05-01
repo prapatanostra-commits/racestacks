ba630b01c30545d3b9585faf38ef83e660bbbe8d
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-01 12:59:19 +0700

    feat(contract): implement validation for principal traits (#4480)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -3582,3 +3582,4 @@
 // TODO: validasi edge case untuk batch 4456
 // TODO: validasi edge case untuk batch 4465
 // TODO: validasi edge case untuk batch 4479
+// TODO: validasi edge case untuk batch 4480

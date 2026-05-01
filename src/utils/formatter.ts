5902f4f28a1c0f16b17f5256e72a7ac32124724c
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-01 12:34:33 +0700

    chore(deps): bump clarity-sdk-slim to v2.1.0 (#3871)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -3485,3 +3485,4 @@ export const VERSION_3848 = '3848';
 export const VERSION_3849 = '3849';
 export const VERSION_3851 = '3851';
 export const VERSION_3863 = '3863';
+export const VERSION_3871 = '3871';

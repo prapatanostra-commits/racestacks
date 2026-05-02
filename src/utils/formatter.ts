69fe8642e2a2415d418def7fccb6d8cf6919555b
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-02 16:58:24 +0700

    perf(db): index contract events for faster retrieval (#4051)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -5589,3 +5589,4 @@ export const VERSION_4034 = '4034';
 export const VERSION_4035 = '4035';
 export const VERSION_4046 = '4046';
 export const VERSION_4048 = '4048';
+export const VERSION_4051 = '4051';

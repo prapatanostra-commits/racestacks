ec3dc777fe4212a7954fdef31d7f1b7a170d031d
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-03 20:21:47 +0700

    docs(readme): update deployment instructions for mainnet (#4249)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -6469,3 +6469,4 @@
 // TODO: validasi edge case untuk batch 4237
 // TODO: validasi edge case untuk batch 4238
 // TODO: validasi edge case untuk batch 4244
+// TODO: validasi edge case untuk batch 4249

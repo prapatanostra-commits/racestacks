602c3ce0bfe29e4d4e1236e71bb4ba59109813b3
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-03 18:04:34 +0700

    docs(readme): update deployment instructions for mainnet (#1014)

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -5975,3 +5975,4 @@ export const VERSION_1005 = '1005';
 export const VERSION_1009 = '1009';
 export const VERSION_1010 = '1010';
 export const VERSION_1013 = '1013';
+export const VERSION_1014 = '1014';

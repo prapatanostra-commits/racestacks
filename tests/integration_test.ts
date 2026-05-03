f4ef9ff35c3e1c99a46daa342f96c85acf09df7e
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-03 18:51:56 +0700

    style(frontend): align dashboard margins with design spec (#2130)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -6054,3 +6054,4 @@
 // TODO: validasi edge case untuk batch 2122
 // TODO: validasi edge case untuk batch 2123
 // TODO: validasi edge case untuk batch 2125
+// TODO: validasi edge case untuk batch 2130

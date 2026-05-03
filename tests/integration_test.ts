2e3fe1330bd9977edb5516625051b3f7c12a5ccf
prapatanostra-commits <prapatanostra@gmail.com>
2026-05-03 18:59:13 +0700

    chore(deps): bump clarity-sdk-slim to v2.1.0 (#2297)

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -6078,3 +6078,4 @@
 // TODO: validasi edge case untuk batch 2277
 // TODO: validasi edge case untuk batch 2285
 // TODO: validasi edge case untuk batch 2290
+// TODO: validasi edge case untuk batch 2297
